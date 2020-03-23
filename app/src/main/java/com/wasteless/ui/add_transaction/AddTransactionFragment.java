43ec788b68ba4db92bd1b7ca99d657de88c85875
Dmitrii <kislenko1910@mail.ru>
2020-03-23 23:02:27 +0200

    New transaction button is added

---
+++ app/src/main/java/com/wasteless/ui/add_transaction/AddTransactionFragment.java
@@ -0,0 +1,20 @@
+package com.wasteless.ui.add_transaction;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.wasteless.R;
+
+public class AddTransactionFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_add_new_transaction, container, false);
+        return root;
+    }
+}
