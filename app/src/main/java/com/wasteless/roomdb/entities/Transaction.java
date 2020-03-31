69d1d34d8df3e3891de3c32c4cec5af64c3a7e35
Joel Ala-aho <43337796+Joqz@users.noreply.github.com>
2020-03-31 20:14:57 +0300

    Removed unnecessary getters

--- app/src/main/java/com/wasteless/roomdb/entities/Transaction.java
+++ app/src/main/java/com/wasteless/roomdb/entities/Transaction.java
@@ -44,15 +44,6 @@ public class Transaction {
     @ColumnInfo(name = "source")
     public String source;
 
-    //COUPLE OF TEST GETTERS
-    public String getType() {
-        return type;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
     //tags are stored in a separate table
 
     //constructor for expense
