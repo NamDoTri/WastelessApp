bcf4ca0e4c96d0468aa8af0ebdd4dd37cf0584a8
Dmitrii <kislenko1910@mail.ru>
2020-03-31 15:14:31 +0300

    Fixed bugs in UI

--- app/src/main/java/com/wasteless/ui/home/goal/GoalFragment.java
+++ app/src/main/java/com/wasteless/ui/home/goal/GoalFragment.java
@@ -1,9 +1,11 @@
 package com.wasteless.ui.home.goal;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
@@ -17,7 +19,15 @@ public class GoalFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
-        View goalFragmentView = inflater.inflate(R.layout.fragment_goal, container, false);
+        final View goalFragmentView = inflater.inflate(R.layout.fragment_goal, container, false);
+        goalFragmentView.findViewById(R.id.submit_goals).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dailyGoal = ((EditText) goalFragmentView.findViewById(R.id.daily_goal)).getText().toString();
+                String weeklyGoal = ((EditText) goalFragmentView.findViewById(R.id.weekly_goal)).getText().toString();
+                Log.i("goals", dailyGoal);
+            }
+        });
 
         return goalFragmentView;
     }
