66c13f92a5d936b809041961a48a98ee10831419
Dmitrii <kislenko1910@mail.ru>
2020-04-01 12:24:55 +0300

    Added first working goal

--- app/src/main/java/com/wasteless/ui/home/goal/GoalFragment.java
+++ app/src/main/java/com/wasteless/ui/home/goal/GoalFragment.java
@@ -6,36 +6,73 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.wasteless.R;
 
+import com.wasteless.roomdb.entities.Goal;
 import com.wasteless.ui.home.HomeViewModel;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class GoalFragment extends Fragment {
     private GoalViewModel goalViewModel;
-
+    ArrayList<String> arrayToDeletePreviousGoals = new ArrayList<String>();
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        goalViewModel = ViewModelProviders.of(this).get(GoalViewModel.class);
         final View goalFragmentView = inflater.inflate(R.layout.fragment_goal, container, false);
+        goalViewModel = ViewModelProviders.of(this).get(GoalViewModel.class);
+        final LinearLayout day_part = goalFragmentView.findViewById(R.id.day_part);
+        final TextView day_goal_text = goalFragmentView.findViewById(R.id.day_goal_text);
+
+        Goal dailyGoal = goalViewModel.getDailyGoal();
+
+        if(goalViewModel.getDailyGoal() != null) {
+            day_part.setVisibility(View.GONE);
+            day_goal_text.setVisibility(View.VISIBLE);
+            day_goal_text.setText("Your goal for today is "+ dailyGoal.amountOfMoney);
+        }
+        day_goal_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                day_part.setVisibility(View.VISIBLE);
+                day_goal_text.setVisibility(View.GONE);
+                arrayToDeletePreviousGoals.add("daily");
+            }
+        });
         goalFragmentView.findViewById(R.id.submit_goals).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String date = takeCurrentDate(goalFragmentView);
                 String dailyGoal = ((EditText) goalFragmentView.findViewById(R.id.daily_goal)).getText().toString();
-                String weeklyGoal = ((EditText) goalFragmentView.findViewById(R.id.weekly_goal)).getText().toString();
-                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-                LocalDateTime now = LocalDateTime.now();
-                String date = dtf.format(now);
-                goalViewModel.insertGoal("month", date, Double.parseDouble(dailyGoal));
+                String monthlyGoal = ((EditText) goalFragmentView.findViewById(R.id.monthly_goal)).getText().toString();
+
+                for(int i = 0; i < arrayToDeletePreviousGoals.size(); i++){
+                    goalViewModel.deleteGoalsByType(arrayToDeletePreviousGoals.get(i));
+                }
+                goalViewModel.insertDailyGoal(date, Double.parseDouble(dailyGoal));
                 Log.i("goals", dailyGoal);
+                GoalFragment goalFragment = new GoalFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment, goalFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
             }
         });
 
         return goalFragmentView;
+    }
+
+    private String takeCurrentDate(View goalFragmentView) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        String date = dtf.format(now);
+        return date;
     }
 }
--- app/src/main/java/com/wasteless/ui/home/goal/GoalViewModel.java
+++ app/src/main/java/com/wasteless/ui/home/goal/GoalViewModel.java
@@ -1,11 +1,11 @@
 package com.wasteless.ui.home.goal;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.lifecycle.AndroidViewModel;
 
 import com.wasteless.repository.GoalRepository;
-import com.wasteless.repository.TransactionRepository;
 import com.wasteless.roomdb.entities.Goal;
 
 public class GoalViewModel extends AndroidViewModel {
@@ -15,8 +15,9 @@ public class GoalViewModel extends AndroidViewModel {
         super(application);
         goalRepository = goalRepository.getGoalRepository(application.getApplicationContext());
     }
-    public void insertGoal(String type, String date, double goal){
-        Goal newGoal = new Goal(type, date, goal);
+
+    public void insertDailyGoal(String date, double goal){
+        Goal newGoal = new Goal("daily", date, goal);
         try{
             goalRepository.insertGoal(newGoal);
 
@@ -24,5 +25,23 @@ public class GoalViewModel extends AndroidViewModel {
             e.printStackTrace();
         }
     }
+    public void deleteGoalsByType(String type){
+        try{
+            goalRepository.deleteGoalByType(type);
 
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public Goal getDailyGoal(){
+        Goal dailyGoal = null;
+        try{
+            dailyGoal = goalRepository.getGoalByType("daily");
+           Log.i("goal", String.valueOf(dailyGoal.amountOfMoney));
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return dailyGoal;
+    }
 }
