48f1bf2e899e931d8fda6f79c85fed9eb91ae8aa
NamDoTri <trinam9999@gmail.com>
2020-04-02 17:15:53 +0300

    Import image and display in view

--- app/src/main/java/com/wasteless/ui/add_transaction/AddTransactionFromReceiptFragment.java
+++ app/src/main/java/com/wasteless/ui/add_transaction/AddTransactionFromReceiptFragment.java
@@ -1,10 +1,18 @@
 package com.wasteless.ui.add_transaction;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts.GetContent;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -13,10 +21,28 @@ import com.wasteless.R;
 public class AddTransactionFromReceiptFragment extends Fragment {
     private AddTransactionViewModel addTransactionViewModel;
 
+    private ImageView previewReceiptImage;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         addTransactionViewModel = ViewModelProviders.of(this).get(AddTransactionViewModel.class);
         final View AddTransactionFromReceiptFragmentView = inflater.inflate(R.layout.fragment_add_transaction_from_gallery, container, false);
 
         return AddTransactionFromReceiptFragmentView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+        previewReceiptImage = (ImageView)getView().findViewById(R.id.preview_receipt);
+
+        ActivityResultLauncher<String> getReceiptImage = prepareCall(new GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri result) {
+                        Log.i("receipt", "Image uri: " + result.toString());
+                        previewReceiptImage.setImageURI(result);
+                    }
+                });
+
+        getReceiptImage.launch("image/*");
     }
 }
