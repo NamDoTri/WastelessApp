3aed07b4b7823f1865af9c9507fd35c3a11d06fe
Dmitrii <kislenko1910@mail.ru>
2020-04-06 18:35:57 +0300

    Added notification when adding new wallet

--- app/src/main/java/com/wasteless/ui/settings/newWallet/NewWalletFragment.java
+++ app/src/main/java/com/wasteless/ui/settings/newWallet/NewWalletFragment.java
@@ -1,5 +1,7 @@
 package com.wasteless.ui.settings.newWallet;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,11 +10,14 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.wasteless.R;
 import com.wasteless.roomdb.AppDatabase;
 import com.wasteless.roomdb.entities.Wallet;
+import com.wasteless.ui.add_transaction.AddTransactionFragment;
+import com.wasteless.ui.settings.SettingsFragment;
 import com.wasteless.ui.settings.SettingsViewModel;
 
 public class NewWalletFragment extends Fragment {
@@ -31,7 +36,26 @@ public class NewWalletFragment extends Fragment {
 
                 if(v.getId() == R.id.add_wallet_button) {
                     settingsViewModel.insertWallet(name, Double.valueOf(initialValue));
+                    successMessage();
                 }
+            }
+            private void successMessage() {
+                // Pop-up message
+                AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
+                alertDialog.setTitle("Done");
+                alertDialog.setMessage("Succesfully added");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                SettingsFragment settingsFragment = new SettingsFragment();
+                                FragmentTransaction changeTheScreen = getFragmentManager().beginTransaction();
+                                changeTheScreen.replace(R.id.nav_host_fragment, settingsFragment);
+                                changeTheScreen.addToBackStack(null);
+                                changeTheScreen.commit();
+                            }
+                        });
+                alertDialog.show();
             }
         });
         return MeFragmentView;
