8e3fd4b11ef6783f704bfcfdbf6c8918555e420d
Dmitrii <kislenko1910@mail.ru>
2020-04-09 18:09:48 +0300

    Added notification

--- app/src/main/java/com/wasteless/MainActivity.java
+++ app/src/main/java/com/wasteless/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wasteless;
 
+import android.app.Notification;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -9,6 +10,8 @@ import com.wasteless.roomdb.AppDatabase;
 import com.wasteless.roomdb.entities.Wallet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,10 +21,21 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private AppDatabase db;
+    private NotificationManagerCompat notificationManagerCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+        Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.bank)
+                .setContentTitle("Something here")
+                .setContentText("Oh you are great")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_SOCIAL)
+                .build();
+        notificationManagerCompat.notify(1, notification);
 
         db = AppDatabase.getAppDatabase(this);
         Log.i("Database", "Wallet " + String.valueOf(db.walletDao().getAll()));
