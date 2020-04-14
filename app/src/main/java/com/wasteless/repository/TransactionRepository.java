7f086104c88517549d6aca4d25c9e037ddb077b9
Dmitrii <kislenko1910@mail.ru>
2020-04-14 18:11:14 +0300

    Achievements can be triggered now

--- app/src/main/java/com/wasteless/repository/TransactionRepository.java
+++ app/src/main/java/com/wasteless/repository/TransactionRepository.java
@@ -3,7 +3,10 @@ package com.wasteless.repository;
 import android.content.Context;
 import android.util.Log;
 
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.wasteless.roomdb.AppDatabase;
 import com.wasteless.roomdb.daos.TagDao;
@@ -13,6 +16,8 @@ import com.wasteless.roomdb.entities.Tag;
 import com.wasteless.roomdb.entities.TagAssociation;
 import com.wasteless.roomdb.entities.Transaction;
 import com.wasteless.roomdb.entities.Wallet;
+import com.wasteless.ui.add_transaction.AddTransactionViewModel;
+import com.wasteless.ui.settings.achievements.AchievementViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +27,7 @@ public class TransactionRepository {
     private final TransactionDao transactionDao;
     private final WalletDao walletDao;
     private final TagDao tagDao;
-
+    private AchievementViewModel achievementViewModel;
     private final String[] CATEGORIES = {"Groceries", "Entertainment", "Rent", "Commute", "Clothes", "Furniture", "Miscellaneous"};
     private final String[] INCOMETYPES = {"Salary", "Gift", "Stolen"};
 
@@ -157,7 +162,7 @@ public class TransactionRepository {
             Wallet currentWallet = walletDao.getWalletById(transaction.wallet);
             currentWallet.balance -= transaction.amount;
             walletDao.updateAll(currentWallet);
-
+//            achievementViewModel.getAllAchievements();
             return true;
         }catch(Exception e){
             e.printStackTrace();
--- app/src/main/java/com/wasteless/ui/settings/achievements/AchievementViewModel.java
+++ app/src/main/java/com/wasteless/ui/settings/achievements/AchievementViewModel.java
@@ -1,27 +1,83 @@
 package com.wasteless.ui.settings.achievements;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelStoreOwner;
 
 import com.wasteless.repository.AchievementRepository;
+import com.wasteless.repository.TransactionRepository;
 import com.wasteless.repository.WalletRepository;
 import com.wasteless.roomdb.entities.Achievement;
+import com.wasteless.roomdb.entities.Wallet;
+import com.wasteless.ui.add_transaction.AddTransactionViewModel;
 import com.wasteless.ui.home.goal.GoalViewModel;
+import com.wasteless.ui.settings.SettingsViewModel;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AchievementViewModel extends AndroidViewModel {
     private AchievementRepository achievementRepository;
+    private WalletRepository walletRepository;
+    private TransactionRepository transactionRepository;
+    public MutableLiveData<Integer> currentlyDisplayWalletIndex;
+    private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public AchievementViewModel(@NonNull Application application) {
         super(application);
         achievementRepository = AchievementRepository.getAchievementRepository(application.getApplicationContext());
+        transactionRepository = TransactionRepository.getTransactionRepository(application.getApplicationContext());
+        walletRepository = walletRepository.getWalletRepository(application.getApplicationContext());
+        currentlyDisplayWalletIndex = new MutableLiveData<>();
+        currentlyDisplayWalletIndex.setValue(-1);
+    }
+
+    public double getTotalExpenseToday() {
+        String today = dateFormatter.format(LocalDateTime.now());
+        Double todayTotalExpense = 0.0;
+        if(currentlyDisplayWalletIndex.getValue() != -1){
+            Wallet currentWallet = walletRepository.getAllWallets().get(currentlyDisplayWalletIndex.getValue());
+            todayTotalExpense = transactionRepository.getTotalExpenseByDate(today, currentWallet.walletId);
+        }else{
+            todayTotalExpense = transactionRepository.getTotalExpenseByDate(today, Long.valueOf(-1));
+        }
+        return todayTotalExpense;
+    }
+    public double getTotalIncomeToday() {
+        String today = dateFormatter.format(LocalDateTime.now());
+        double todayTotalIncome = 0.0;
+        if(currentlyDisplayWalletIndex.getValue() != -1){
+            Wallet currentWallet = walletRepository.getAllWallets().get(currentlyDisplayWalletIndex.getValue());
+            todayTotalIncome = transactionRepository.getTotalIncomeByDate(today, currentWallet.walletId);
+        }else{
+            todayTotalIncome = transactionRepository.getTotalIncomeByDate(today, Long.valueOf(-1));
+        }
+        return todayTotalIncome;
     }
 
     public List<Achievement> getAllAchievements(){
         return achievementRepository.getAllAchievements();
+    }
+    public void checkAllTheAchievements(){
+        double todaysIncome = getTotalIncomeToday();
+        double todaysExpense = getTotalExpenseToday();
+        List<Achievement> allAchievements;
+        allAchievements = getAllAchievements();
+        if (!achievementRepository.getAchievementByName("Rich boy").isDone && todaysExpense > 5.0){
+            achievementRepository.setAchievementToBeDone("Rich boy");
+        }
+        if (!achievementRepository.getAchievementByName("Poor guy").isDone && todaysIncome < 5.0){
+            achievementRepository.setAchievementToBeDone("Poor guy");
+        }
+
     }
 }
