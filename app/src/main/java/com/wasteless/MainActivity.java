9e5b5224e5a130c44f7566ab5cfec247bd7b678b
NamDoTri <trinam9999@gmail.com>
2020-04-16 12:24:58 +0300

    Added static password in Main Activity & Validate pattern works

--- app/src/main/java/com/wasteless/MainActivity.java
+++ app/src/main/java/com/wasteless/MainActivity.java
@@ -3,13 +3,18 @@ package com.wasteless;
 import android.app.Notification;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.andrognito.patternlockview.PatternLockView;
+import com.andrognito.patternlockview.listener.PatternLockViewListener;
+import com.andrognito.patternlockview.utils.PatternLockUtils;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.FirebaseApp;
 import com.wasteless.roomdb.AppDatabase;
 import com.wasteless.roomdb.entities.Wallet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.navigation.NavController;
@@ -21,6 +26,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private AppDatabase db;
+    private static String currentPassword = "012";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,26 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Database", "Income " + String.valueOf(db.transactionDao().getAllIncomes()));
 
         setContentView(R.layout.activity_main);
+
+        PatternLockView patternView = findViewById(R.id.password_view);
+        ConstraintLayout defaultMain = findViewById(R.id.default_main);
+        patternView.addPatternLockListener(new PatternLockViewListener() {
+            @Override
+            public void onStarted() {}
+            @Override
+            public void onProgress(List<PatternLockView.Dot> progressPattern) {}
+            @Override
+            public void onComplete(List<PatternLockView.Dot> pattern) {
+                String currentPattern = PatternLockUtils.patternToString(patternView, pattern);
+                if(MainActivity.validatePassword(currentPattern)){
+                    patternView.setVisibility(View.GONE);
+                    defaultMain.setVisibility(View.VISIBLE);
+                }
+            }
+            @Override
+            public void onCleared() {}
+        });
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -45,4 +71,10 @@ public class MainActivity extends AppCompatActivity {
         FirebaseApp.initializeApp(this);
     }
 
+    public static String getCurrentPassword(){
+        return currentPassword;
+    }
+    public static boolean validatePassword(String newPassword){
+        return newPassword.equalsIgnoreCase(currentPassword);
+    }
 }
